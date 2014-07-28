5a52c05efaa16b13c3904dc2998772eaf3d3efee
David Federspiel <gitter@federnet.com>
2014-07-28 10:39:17 -0400

    Refactor and Feature Enhancements
    Refactored code and made playing pieces dynamic.

--- craps/src/craps/Craps.java
+++ craps/src/craps/Craps.java
@@ -5,20 +5,21 @@
  */
 package craps;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author dfederspiel
- */
+// Main application game loop
 public class Craps {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create the new game object
         Game game = new Game();
+
+        // Add the playing pieces (can be any number and with n number of sides)
+        game.addDie(new Die(4));
+        game.addDie(new Die(4));
+        game.addDie(new Die(4));
         while (!game.isOver) {
             game.placeBet();
             game.roll();
@@ -27,24 +28,49 @@ public class Craps {
     }
 }
 
+// Represents a single die
+class Die {
+
+    int numSides;
+    int sidesRolled[];
+    int value;
+
+    public Die(int sides) {
+        this.numSides = sides;
+        this.sidesRolled = new int[numSides];
+    }
+
+    public int roll() {
+        int result = (int) (Math.random() * this.numSides);
+        sidesRolled[result]++;
+        this.value = result + 1;
+        return this.value;
+    }
+}
+
+// Game object
 class Game {
 
     boolean isOver = false;
     int pot = 50;
-    int diceOne = 0;
-    int diceTwo = 0;
+    int valueMetrics[];
+
+    private ArrayList<Die> dice = new ArrayList<>();
+
     int bet = 0;
-    int wins = 0;
-    int loss = 0;
+    int calculateWins = 0;
+    int losses = 0;
     int point = 0;
     int option = 0;
-    int d1 = 0, d2 = 0, d3 = 0, d4 = 0, d5 = 0, d6 = 0;
-    int dt2 = 0, dt3 = 0, dt4 = 0, dt5 = 0, dt6 = 0, dt7 = 0, dt8 = 0, dt9 = 0, dt10 = 0, dt11 = 0, dt12 = 0;
 
+    int minRoll = 0;
+    int maxRoll = 0;
+
+  //int d1 = 0, d2 = 0, d3 = 0, d4 = 0, d5 = 0, d6 = 0;
+    //int dt2 = 0, dt3 = 0, dt4 = 0, dt5 = 0, dt6 = 0, dt7 = 0, dt8 = 0, dt9 = 0, dt10 = 0, dt11 = 0, dt12 = 0;
     public void placeBet() {
         String betString = JOptionPane.showInputDialog(null, "LET'S PLAY CRAPS!\n\n Your pot is $ " + pot
                 + "\n\nHow much do you want to bet?", "Enter a bet", JOptionPane.QUESTION_MESSAGE);
-
         try {
             bet = Integer.parseInt(betString);  //convert string to an integer
         } catch (Exception e) {
@@ -56,143 +82,122 @@ class Game {
             JOptionPane.showInputDialog(null, "Your bet is too high, \nYour pot is $ " + pot + "\nEnter a valid bet", "Enter a valid bet", JOptionPane.QUESTION_MESSAGE);
             placeBet();
         }
-
     }
 
+  // Add die to game.  This is important as it will calculate minimum and maximum values, create
+    // the valueMetrics variable which is an array, resized to the possible roll values, and then used to track
+    // how many times a value has been hit.
+    public void addDie(Die die) {
+        this.dice.add(die);
+        this.minRoll = dice.size();
+        this.maxRoll = 0;
+        for (int i = 0; i < dice.size(); i++) {
+            this.maxRoll += dice.get(i).numSides;
+        }
+        valueMetrics = new int[(this.maxRoll - this.minRoll) + 1];
+    }
+
+    // Perform dice roll (aka, take a turn)
     public void roll() {
 
         rollDice();
+        point = getTotal();
+        valueMetrics[point - 1]++;
 
-        point = diceOne + diceTwo;
-
-        if ((diceOne + diceTwo == 7) || (diceOne + diceTwo == 11)) {
-            win();
-        } else if ((diceOne + diceTwo == 2) || (diceOne + diceTwo == 3) || (diceOne + diceTwo == 12)) {
-            lose();
+        if ((point == 7) || (point == 11)) {
+            calculateWin();
+        } else if ((point == 2) || (point == 3) || (point == 12)) {
+            calculateLoss();
         } else {
             point();
         }
     }
 
-    public void win() {
-        wins++;
+    // Calculate the win scenario
+    public void calculateWin() {
+        calculateWins++;
         pot += bet;
-        JOptionPane.showMessageDialog(null, "You rolled " + (diceOne + diceTwo) + " YOU WIN! \n\nYour pot is $ " + pot);
-
+        JOptionPane.showMessageDialog(null, "You rolled " + getTotal() + " YOU WIN! \n\nYour pot is $ " + pot);
     }
 
-    public void lose() {
-        loss++;
+    // Calculate loss Scenario
+    public void calculateLoss() {
+        losses++;
         pot -= bet;
-        JOptionPane.showMessageDialog(null, "You rolled " + (diceOne + diceTwo) + " YOU LOSE! \n\nYour pot is $ " + pot);
-
+        JOptionPane.showMessageDialog(null, "You rolled " + getTotal() + " YOU LOSE! \n\nYour pot is $ " + pot);
     }
 
+    // Calculate 'point' scenario
     public void point() {
-        JOptionPane.showMessageDialog(null, "You rolled " + (diceOne + diceTwo) + " (" + diceOne
-                + " and " + diceTwo + ") \n\nYou need " + point + " to win \n\nROLL AGAIN");
+        JOptionPane.showMessageDialog(null, "You rolled " + getTotal() + " (" + getIndividualValueString() + ") \n\nYou need " + point + " to calculateWin \n\nROLL AGAIN");
         rollDice();
-        if (diceOne + diceTwo == 7) {
-            lose();
-        } else if (diceOne + diceTwo == point) {
-            win();
+        if (getTotal() == 7) {
+            calculateLoss();
+        } else if (getTotal() == point) {
+            calculateWin();
         } else {
             point();
         }
 
     }
 
+    // Play again dialog.
     public void playAgain() {
         option = JOptionPane.showConfirmDialog(null, "Do you want to play again?");
         if ((option == 1) || (option == 2)) {  //for NO and CANCEL options
             isOver = true;
-            JOptionPane.showMessageDialog(null, "You WON " + wins + " time(s) \n\nYou LOST " + loss + " time(s)"
-                    + "\n\nYour total pot is $ " + pot + "\n\nStatistics Single Dice:\nOnes: " + d1
-                    + "\nTwos: " + d2 + "\nThrees: " + d3 + "\nFours: " + d4 + "\nFives: " + d5
-                    + "\nSixes: " + d6 + "\n\nStatistics Dice Total:\nTwo: " + dt2 + "\nThree: " + dt3
-                    + "\nFour: " + dt4 + "\nFive: " + dt5 + "\nSix: " + dt6 + "\nSeven: " + dt7
-                    + "\nEight: " + dt8 + "\nNine: " + dt9 + "\nTen: " + dt10 + "\nEleven: " + dt11
-                    + "\nTwelve: " + dt12 + "\n\nThank you for playing!");
+            StringBuilder stats = new StringBuilder();
+            stats.append("You WON " + calculateWins + " time(s) \n\nYou LOST " + losses + " time(s)");
+            stats.append("\n\nYour total pot is $ " + pot + "\n\nStatistics Single Dice:\n");
+
+            // First append all the individual dice and their individual stats, listing Die#, Sides, and counts for each.
+            for (int i = 0; i < dice.size(); i++) {
+                stats.append("Die #" + (i + 1) + "\n");
+                for (int x = 0; x < dice.get(i).sidesRolled.length; x++) {
+                    stats.append("S:" + (x + 1) + "-R" + dice.get(i).sidesRolled[x] + "\n");
+                }
+                stats.append("\n");
+            }
+
+            // Then look at each value in the value metrics and list the number of times a value was hit.
+            for (int i = 0; i < valueMetrics.length; i++) {
+                stats.append("V:" + (minRoll + i) + "-T:" + valueMetrics[i] + "\n");
+            }
+
+            JOptionPane.showMessageDialog(null, stats.toString());
         }
     }
 
+    // Call the roll method of each die in the dice collection.
     public void rollDice() {
-        diceOne = (int) (Math.random() * 6 + 1);
-        diceTwo = (int) (Math.random() * 6 + 1);
-        stats();
-
+        for (int i = 0; i < dice.size(); i++) {
+            dice.get(i).roll();
+        }
     }
 
-    public void stats() {
-        if (diceOne == 1) {
-            d1++;
+    // Gets a string with all rolled values
+    public String getIndividualValueString() {
+        String returnValue = "";
+        for (int i = 0; i < dice.size(); i++) {
+            returnValue += dice.get(i).value;
+            if (i + 1 < dice.size()) {
+                returnValue += ", ";
+            }
         }
-        if (diceOne == 2) {
-            d2++;
-        }
-        if (diceOne == 3) {
-            d3++;
-        }
-        if (diceOne == 4) {
-            d4++;
-        }
-        if (diceOne == 5) {
-            d5++;
-        }
-        if (diceOne == 6) {
-            d6++;
-        }
-        if (diceTwo == 1) {
-            d1++;
-        }
-        if (diceTwo == 2) {
-            d2++;
-        }
-        if (diceTwo == 3) {
-            d3++;
-        }
-        if (diceTwo == 4) {
-            d4++;
-        }
-        if (diceTwo == 5) {
-            d5++;
-        }
-        if (diceTwo == 6) {
-            d6++;
-        }
-        if (diceOne + diceTwo == 2) {
-            dt2++;
-        }
-        if (diceOne + diceTwo == 3) {
-            dt3++;
-        }
-        if (diceOne + diceTwo == 4) {
-            dt4++;
-        }
-        if (diceOne + diceTwo == 5) {
-            dt5++;
-        }
-        if (diceOne + diceTwo == 6) {
-            dt6++;
-        }
-        if (diceOne + diceTwo == 7) {
-            dt7++;
-        }
-        if (diceOne + diceTwo == 8) {
-            dt8++;
-        }
-        if (diceOne + diceTwo == 9) {
-            dt9++;
-        }
-        if (diceOne + diceTwo == 10) {
-            dt10++;
-        }
-        if (diceOne + diceTwo == 11) {
-            dt11++;
-        }
-        if (diceOne + diceTwo == 12) {
-            dt12++;
-        }
+        return returnValue;
+    }
 
+    // Gets a grand total of each of the die in the dice collection.
+    public int getTotal() {
+        int total = 0;
+        for (int i = 0; i < dice.size(); i++) {
+            total += dice.get(i).value;
+        }
+        return total;
+    }
+
+    // Get total for a specific die by passing in an index.
+    public int getTotal(int idx) {
+        return dice.get(idx).value;
     }
 }
